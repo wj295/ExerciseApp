8cc165ddba80e05c3f74255ecbe1d4cfe9e86ecc
JIUN <zin295@yahoo.com.tw>
2016-11-21 00:28:11 +0800

    Ex1

--- app/src/main/java/com/example/wj/exerciseapp/MainActivity.java
+++ app/src/main/java/com/example/wj/exerciseapp/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.wj.exerciseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     //Test 20161120
     public void buttonClick(View view) {
+        String tmp = getString(R.string.MsgTest2);
+        String tmp2 = getString(R.string.app_name);
+        toast = Toast.makeText(this, test(tmp) + tmp2, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
+    private String test(String str)
+    {
+        return "GG: " + str;
     }
 }
